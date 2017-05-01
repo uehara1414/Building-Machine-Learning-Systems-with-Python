2d8bb2579b20ff74ce3a3f580e8305a3d0bb2b6e
uehara <b1015062@fun.ac.jp>
2017-05-01 12:55:20 +0900

    update

--- chapter1/1.py
+++ chapter1/1.py
@@ -1,6 +1,11 @@
 import matplotlib.pyplot as plt
 import scipy as sp
 
+
+def error(f, x, y):
+    return sp.sum((f(x) - y) ** 2)
+
+
 def main():
 
     data = sp.genfromtxt('data/web_traffic.tsv', delimiter='\t')
@@ -9,6 +14,13 @@ def main():
 
     x = x[~sp.isnan(y)]
     y = y[~sp.isnan(y)]
+
+    fp1, residuals, rank, sv, rcond = sp.polyfit(x, y, 1, full=True)
+    f1 = sp.poly1d(fp1)
+
+    fx = sp.linspace(0, x[-1], 1000)
+    plt.plot(fx, f1(fx), linewidth=4)
+    plt.legend([f"d={f1.order}"], loc='upper left')
 
     plt.scatter(x, y)
     plt.title('Web traffic over the world')
